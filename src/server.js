b9fe9b9f43d73cefc90f1b722051ed03ad2896ab
Obayed Mamur Zihan <obayedmamur@gmail.com>
2021-06-24 21:24:45 +0600

    Route Handled

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 3000;
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Established Mongoose Connection");
+});
+
+const router = require("./routes/routes");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port ${PORT}`);
+});
