bcd8b09d30a02645b556d51323521909d5e90543
Obayed Mamur Zihan <obayedmamur@gmail.com>
2021-06-25 15:25:59 +0600

    Existing User Verification

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,14 +8,22 @@ router.get("/", (req, res) => {
 
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    console.log("Registration Successful!");
-    res.json({ message: "Registration Successful!", status: 201 });
+    const existingUser = User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      const newUser = new User(req.body);
+      await newUser.save();
+      console.log("Registration Successful!");
+      res.json({ message: "Registration Successful!", status: 201 });
+    } else {
+      console.log(
+        `Aborting user registration as user already registered with this email: ${req.body.email}`
+      );
+      res.status(400).json({ message: "Email already in use!", status: 400 });
+    }
   } catch (e) {
     console.log("Registration Failed!");
     console.log("Error details: ", e);
-    res.json({ message: "Registration failed!", status: 500 });
+    res.status(500).json({ message: "Registration failed!", status: 500 });
   }
 });
 
